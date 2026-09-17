58dd596b8fdc773d61cce5493453b224db174a06
Cindy Isip <cisip16@acsicorp.com>
2026-09-17 16:39:40 -0700

    Adding buildspec.yml

--- commandsV2.sh
+++ commandsV2.sh
@@ -6,6 +6,9 @@ export REPO_NAME="dea8-dev-west1-ecr-calendly-01"
 #export AWS_ACCOUNT_ID="952933884450"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
+# 1. Build image for ECR repository
+docker build --platform linux/amd64 -t calendly .
+
 # 2. Tag the local 'calendly' image for your remote ECR repository
 docker tag calendly:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/${REPO_NAME}:latest
 
